185f2561fd716c80704a7335a7b645dfcb23f6cc
DiegoCentanni <centanni.diego@iisgalvanimi.edu.it>
2026-01-23 09:39:18 +0000

    Esercizio 1 Completo

--- java.js
+++ java.js
@@ -1,21 +1,22 @@
-function Saluta(){
-    let nome = document.getElementById("Nome").value;
+function SalutaFN(){
+    let nome = document.getElementById("Nome").value; 
     let genere = document.getElementById("Genere").value;
-    let messaggio = document.getElementById("Messaggio").value;
+    let messaggio = document.getElementById("MessaggioDiBenvenuto");
     let inputnome = document.getElementById("Nome");
-
-    if (Nome === "" ){
-        Messaggio.innerText = "Inserire un nome";
+    if (nome === "" ){
+        document.getElementById("MessaggioDiBenvenuto").innerText = "Inserire un nome";
         inputnome.style.border = "2px solid red";
     }
-    if (Genere != Uomo || Genere != Donna){
-        Messaggio.innerText = "Inserire un genere valido!";
+    if (genere != "Uomo" && genere != "Donna"){
+        messaggio.innerText = "Inserire un genere valido!";
+        inputnome.style.border = "2px solid red"
     }
-    
-
-
-
-
-
-
+    else if (genere === "Uomo"){
+        messaggio.innerText = "Ciao " + nome + " Benevenuto!"
+        messaggio.style.color = "blue"
+    }
+    else if (genere === "Donna"){
+         messaggio.innerText = "Ciao " + nome + " Benvenuta!"
+         messaggio.style.color= "pink"
+    }
 }
